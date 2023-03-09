707af77f0a9a51ebde8f93b71f7975ecdc9f2feb
Gustavo Luche <gustavo.luche@hotmail.com>
2023-03-09 17:53:37 -0300

    :tada: Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  // if (html.classList.contains("light")) {
+  //   html.classList.remove("light")
+  // } else {
+  //   html.classList.add("light")
+  // }
+
+  html.classList.toggle("light")
+
+  // const img = document.querySelector("#profile img")
+
+  // if (html.classList.contains("light")) {
+  //   img.setAttribute("src", "assents/avatar-2.png")
+  // } else {
+  //   img.setAttribute("src", "assents/avatar-1.png")
+  // }
+}
